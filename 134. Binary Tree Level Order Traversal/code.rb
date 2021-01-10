542d7debccff4cf16e6a98daa49be3a37a3fa711
erdees <rds@rds.prv>
2021-01-10 21:08:14 +0100

    add new solution

---
+++ 134. Binary Tree Level Order Traversal/code.rb
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[][]}
+def level_order(root)
+  out ||= []
+  level = 0
+  out = level_order_helper(root, out, level)  
+end
+
+def level_order_helper(root, out, level)
+  if root&.val
+    if out[level]
+      out[level].push(root.val)
+    else
+      out[level] = [root.val]
+    end
+    level += 1
+    level_order_helper(root.left, out, level) if root.left
+    level_order_helper(root.right, out, level) if root.right
+  end
+  out
+end
